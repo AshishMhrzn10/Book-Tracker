074f6bb8098633fd0eac59bb85539d0aaff53e2f
Ashish Maharjan <amaharjan1032@gmail.com>
2022-06-11 17:13:24 +0545

    Models created

---
+++ BookTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookTracker.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookTracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+          
+        }
+        public DbSet<Book> Book { get; set; }
+    }
+}
